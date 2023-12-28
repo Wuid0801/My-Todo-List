612f27043a7fe63575d6f260c6deac9fc0b442bf
Kang HyeSeong <pig10326@naver.com>
2023-12-28 11:01:03 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState(''); 
+  const [detail, setDetail] = useState([
+    { id: 1, detail: "리액트를 배워봅시다." },
+    { id: 2, detail: "리액트를 배워봅시다." }
+  ]);
+
+  const clickAddButtonHandler = () => {
+    const newDetail = {
+      id: uuidv4(),
+      detail: input, 
+    };
+
+    setDetail([...detail, newDetail]);
+    setInput('');  
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="input-style">
+        <div className='input-container'>
+          <input
+            placeholder="내용을 입력해주세요"
+            value={input} 
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <button onClick={clickAddButtonHandler}>추가</button>
+        </div>
+        <h1>ToDo 목록</h1>
+      </div>
+      <div className='detail-style'>
+        {detail.map((event) => (
+          <div className="detail-card" key={event.id}>
+            <h2>{event.detail}</h2>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
